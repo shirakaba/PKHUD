63856057a10ab207fdba73b92a81aa7712823acd
shirakaba <14055146+shirakaba@users.noreply.github.com>
2018-07-14 22:52:41 +0100

    Update PKHUD project to Swift 4.0

--- PKHUD/PKHUD.swift
+++ PKHUD/PKHUD.swift
@@ -171,7 +171,7 @@ open class PKHUD: NSObject {
 
     // MARK: Internal
 
-    internal func willEnterForeground(_ notification: Notification?) {
+    @objc internal func willEnterForeground(_ notification: Notification?) {
         self.startAnimatingContentView()
     }
 
@@ -189,7 +189,7 @@ open class PKHUD: NSObject {
 
     // MARK: Timer callbacks
 
-    internal func performDelayedHide(_ timer: Timer? = nil) {
+    @objc internal func performDelayedHide(_ timer: Timer? = nil) {
         let userInfo = timer?.userInfo as? [String:AnyObject]
         let key = userInfo?["timerActionKey"] as? String
         var completion: TimerAction?
@@ -202,7 +202,7 @@ open class PKHUD: NSObject {
         hide(animated: true, completion: completion)
     }
 
-    internal func handleGraceTimer(_ timer: Timer? = nil) {
+    @objc internal func handleGraceTimer(_ timer: Timer? = nil) {
         // Show the HUD only if the task is still running
         if (graceTimer?.isValid)! {
             showContent()
